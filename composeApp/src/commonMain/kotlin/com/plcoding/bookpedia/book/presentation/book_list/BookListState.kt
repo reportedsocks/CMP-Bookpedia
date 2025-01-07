e5cdecd5b60d8922bfa8e845cd2ff6a610f55458
Daniil Antsyferov <d.antsyferov@devlight.io>
2025-01-07 17:51:38 +0100

    Setup networking

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -5,25 +5,9 @@ import com.plcoding.bookpedia.core.presentation.UiText
 
 data class BookListState(
     val searchQuery: String = "Kotlin",
-    val searchResults: List<Book> = books,
+    val searchResults: List<Book> = emptyList(),
     val favouriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null
 )
-
-val books = (1..100).map {
-    Book(
-        id = it.toString(),
-        title = "Book $it",
-        imageUrl = "https://test",
-        authors = listOf("Daniil Antsyferov"),
-        description = "Description $it",
-        languages = emptyList(),
-        firstPublishYear = null,
-        averageRating = 3.5777,
-        ratingCount = 10,
-        numPages = null,
-        numEditions = 1
-    )
-}
